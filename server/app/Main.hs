910fbda8cb0ffaa271ec27fd9eaa66c41f2a881b
Daiji Yamashita <yamashita.daiji@gmail.com>
2022-06-06 22:25:17 +0900

    add websocket pings

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -17,20 +17,30 @@ import System.IO
 import Debug.Trace (trace)
 
 import Data.Text (Text)
+import Data.IORef
 
 import Control.Monad (forever)
 import Control.Exception.Safe
+import Control.Concurrent (forkIO, threadDelay, killThread)
 
 immediatePutStrLn :: String -> IO ()
 immediatePutStrLn text = do
   putStrLn text
   hFlush stdout
 
-websocketsServerApp :: WebSockets.ServerApp
-websocketsServerApp pendingConnection = do
-  immediatePutStrLn "Websokets server responding!"
+immediatePutStr :: String -> IO ()
+immediatePutStr text = do
+  putStr text
+  hFlush stdout
+
+websocketsServerApp :: IORef Int -> WebSockets.ServerApp
+websocketsServerApp ref pendingConnection = do
+  count <- readIORef ref
+  immediatePutStrLn $ "Websokets server responding!, count: " ++ show count
   hFlush stdout
   connection <- WebSockets.acceptRequest pendingConnection
+  threadId <- forkIO $ sendPingLoop connection
+  atomicModifyIORef ref (\count -> (count+1, ()))
   result <- try $ forever $ do
     message <- WebSockets.receiveData connection :: IO Text
     immediatePutStrLn $ show message
@@ -38,22 +48,32 @@ websocketsServerApp pendingConnection = do
     Left (err::SomeException) -> immediatePutStrLn ("error: " ++ show err)
     Right _ -> immediatePutStrLn "websocket connection closed successfully"
     _       -> immediatePutStrLn "???"
+  atomicModifyIORef ref (\count -> (count-1, ()))
+  killThread threadId
   
+sendPingLoop :: WebSockets.Connection -> IO ()
+sendPingLoop connection = do
+  immediatePutStrLn "start pinging..."
+  forever $ do
+    threadDelay 1000000
+    WebSockets.sendPing connection ("ping" :: Text)
 
 normalServerApp :: Wai.Application
 normalServerApp _ respond = do
   immediatePutStrLn "Normal server responding!"
   respond $ Wai.responseFile status200 [("Content-Type", "text/html")] "../client/index.html" Nothing
 
-router :: Wai.Application
-router req
-  | null path = mainApp req
+router :: IORef Int -> Wai.Application
+router ref req
+  | null path = mainApp ref req
   | otherwise = trace (show path) (staticServer req)
     where
       path = Wai.pathInfo req
 
-mainApp :: Wai.Application
-mainApp = websocketsOr WebSockets.defaultConnectionOptions websocketsServerApp normalServerApp
+mainApp :: IORef Int -> Wai.Application
+mainApp ref = websocketsOr connectionOptions (websocketsServerApp ref) normalServerApp
+  where
+    connectionOptions = WebSockets.defaultConnectionOptions 
 
 staticServer :: Wai.Application
 staticServer = staticApp $ settings { ssIndices = indices }
@@ -62,5 +82,7 @@ staticServer = staticApp $ settings { ssIndices = indices }
     indices = fromJust $ toPieces ["index.html"]
 
 main :: IO ()
-main = Warp.run 8080 router
+main = do
+  ref <- newIORef 0
+  Warp.run 8080 $ router ref
 
